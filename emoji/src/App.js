3545f9f01b5bd0e6bd2c8bd23f6f6d5df755588a
Sam Cross <SamCross@Sams-MBP-5.flashtalking.lds>
2017-11-06 11:46:02 +0000

    filter emiji search

--- emoji/src/App.js
+++ emoji/src/App.js
@@ -10,22 +10,35 @@ class App extends Component {
         super(props);
         //set states
         this.state = {
-            list: emojiList,
+            list: [],
             input: ''
         }
         //setuping your own custom functions
         this.inputHandler = this.inputHandler.bind(this);
     }
     inputHandler (event) {
-      //updating the state/value of input 
+      //updating the state/value of input
+      var input = event.target.value.toLowerCase();
+
+      var newEmojiList = emojiList.filter(function (emojiObject) {
+        if (emojiObject.keywords.includes(input)) {
+          if (input === '') return;
+          else return emojiObject;
+        }
+      })
+      console.log(newEmojiList);
       this.setState({
-        input : event.target.value
-      })  
+        input: input,
+        list : newEmojiList
+      }) 
     }
     render () {
         return (
             <div className="app">
-                <input className="searchBar" placeHolder="Search Emoji" onChange={this.inputHandler}/>
+                <input className="searchBar"
+                 placeholder="Search Emoji" 
+                 onChange={this.inputHandler}
+                 />
                 <Emojis parentState={this.state.list}/>
             </div>
         )
