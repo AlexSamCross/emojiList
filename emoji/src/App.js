dcab324181878a864a5762545eb11ceb88e624e5
Sam Cross <SamCross@Sams-MBP-5.flashtalking.lds>
2017-11-09 11:29:22 +0000

    adding coping feature

--- emoji/src/App.js
+++ emoji/src/App.js
@@ -8,38 +8,60 @@ import emojiList from './emojiList.json'
 class App extends Component { 
     constructor (props) {
         super(props);
-        //set states
+        //set statess
         this.state = {
             list: [],
             input: ''
         }
         //setuping your own custom functions
         this.inputHandler = this.inputHandler.bind(this);
+        this.handleEmojiClick = this.handleEmojiClick.bind(this);
     }
+    //methods
     inputHandler (event) {
-      //updating the state/value of input
+
       var input = event.target.value.toLowerCase();
 
       var newEmojiList = emojiList.filter(function (emojiObject) {
         if (emojiObject.keywords.includes(input)) {
-          if (input === '') return;
+          if (input === '') return; 
           else return emojiObject;
         }
       })
-      console.log(newEmojiList);
+
       this.setState({
         input: input,
         list : newEmojiList
       }) 
+
     }
+    handleEmojiClick (event) {
+      var selectedEmoji = event.target.innerHTML;
+      var emojiElement = event.target;
+
+      emojiElement.style.backgroundColor = "red";
+      //window.prompt("Copy to clip board: Ctrl+C Enter", selectedEmoji)
+      var textArea = document.createElement('textarea');
+      textArea.textContent = selectedEmoji;
+      document.body.appendChild(textArea);
+      textArea.select();
+
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+    }
+
     render () {
         return (
             <div className="app">
-                <input className="searchBar"
-                 placeholder="Search Emoji" 
-                 onChange={this.inputHandler}
-                 />
-                <Emojis parentState={this.state.list}/>
+                <input 
+                  className="searchBar"
+                  placeholder="Search Emoji" 
+                  onChange={this.inputHandler}
+                />
+                <Emojis 
+                parentState ={ this.state.list }
+                handleEmojiClick ={this.handleEmojiClick}
+                />
             </div>
         )
     }
