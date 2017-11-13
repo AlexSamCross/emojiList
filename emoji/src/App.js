9655fb384434713f888e326d0cf98452038668ed
Sam Cross <SamCross@Sams-MBP-5.flashtalking.lds>
2017-11-13 11:50:16 +0000

    adding css and bug fixes

--- emoji/src/App.js
+++ emoji/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Emojis from './Emojis.js';
 import emojiList from './emojiList.json';
-import { Debounce } from 'react-throttle';
-
+import { debounced, copyToClipBoard } from './helpers.js';
+import Notifications, {notify} from 'react-notify-toast';
 
 class App extends Component { 
     constructor (props) {
@@ -11,7 +11,13 @@ class App extends Component {
         //set statess
         this.state = {
             list: [],
-            input: ''
+            input: '',
+            currentEmoji: '',
+            debounced: debounced(function (EmojiList) {
+              this.setState({
+                list: EmojiList
+              })
+            }, 750, this)
         }
         //setuping your own custom functions
         this.inputHandler = this.inputHandler.bind(this);
@@ -19,51 +25,64 @@ class App extends Component {
     }
     //methods
     inputHandler (event) {
-
       var input = event.target.value.toLowerCase();
-
       var newEmojiList = emojiList.filter(function (emojiObject) {
         if (emojiObject.keywords.includes(input)) {
           if (input === '') return; 
           else return emojiObject;
         }
       });
-
-      this.setState({
-        input: input,
-        list : newEmojiList
-      }) 
-
+      //set statess of method
+      this.setState({ input: input }) 
+      // call debounce function to add delay.
+      this.state.debounced(newEmojiList);
     }
+
     handleEmojiClick (event) {
-      var selectedEmoji = event.target.innerHTML;
-      var emojiElement = event.target;
+      
+      var selectedEmoji = event.target.innerHTML,
+        emojiElement = event.target;
+      //remove styles
+      if (this.state.currentEmoji){
+        this.state.currentEmoji.removeAttribute('style');
+      }
+      //set style
+      emojiElement.style.backgroundColor = "green";
 
-      emojiElement.style.backgroundColor = "red";
-      //window.prompt("Copy to clip board: Ctrl+C Enter", selectedEmoji)
-       var textArea = document.createElement('textarea');
-       textArea.textContent = selectedEmoji;
-       document.body.appendChild(textArea);
-       textArea.select();
-
-       document.execCommand('copy');
-       document.body.removeChild(textArea);
+      copyToClipBoard(selectedEmoji);
+      notify.show('Copied to ClipBoard!');
+      this.setState({ currentEmoji: emojiElement });
+      
     }
-
+    //render app
     render () {
         return (
-            <div className="app">
-                <Debounce time="300" handler="onChange"> 
-                  <input 
-                    className="searchBar"
-                    placeholder="Search Emoji" 
-                    onChange={this.inputHandler}
-                  />
-                </Debounce>
-                <Emojis 
-                  parentState ={this.state.list}
-                  handleEmojiClick ={this.handleEmojiClick}
-                />
+            <div>
+                <Notifications />
+                <section className="hero is-small is-primary">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                            👻💩🐔 &nbsp; Emoji Funtime! &nbsp; 😁😆😍
+                            </h1>
+                        </div>
+                    </div>
+                </section>
+
+                <div className="main">
+                    <input 
+                        className="input"
+                        placeholder="Type something"
+                        onChange={this.inputHandler}
+                        value={this.state.input}
+                    />
+                    <div id="emojis">
+                        {<Emojis 
+                            parentState={this.state.list}
+                            handleEmojiClick={this.handleEmojiClick}
+                        />}
+                    </div>
+                </div>
             </div>
         )
     }
